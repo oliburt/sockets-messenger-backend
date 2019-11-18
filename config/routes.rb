57b1d99d7195fa0746c6717fb68e90ecd2895481
oliburt <oliburt@gmail.com>
2019-11-18 13:11:08 +0000

    added controllers and changed routes for api/v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
-  resources :messages
-  resources :chatrooms
-  resources :users
+  namespace :api do
+    namespace :v1 do
+        resources :messages
+        resources :chatrooms
+        resources :users
 
-  
+        post "/login", to: "auth#create"
+        post "/signup", to: "users#create"
+        post "/validate", to: "auth#validate"
+      end
+  end
+
+
+
 end

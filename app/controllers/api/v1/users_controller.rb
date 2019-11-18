74055b98df906fb94c6519d81ab7c84658b0fe31
oliburt <oliburt@gmail.com>
2019-11-18 13:24:39 +0000

    create user for signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: { token: issue_token({user_id: user.id}), user: UserSerializer.new(user) }
+        else
+            render json: { errors: user.errors.full_messages }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+
 end

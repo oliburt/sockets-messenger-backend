daf8b307223ce57a9e09328a7a213a686f16143a
oliburt <oliburt@gmail.com>
2019-11-18 12:57:05 +0000

    added user, chatroom and message models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+User.destroy_all 
+Chatroom.destroy_all
+
+
+chatrooms = Chatroom.create([{name: "Global", description: "TEST"}])
+

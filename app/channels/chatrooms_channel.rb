8af8012b3e209d1f6b694bf85b6976413f3ed950
oliburt <oliburt@gmail.com>
2019-11-20 17:41:31 +0000

    private channel for current_user

--- app/channels/chatrooms_channel.rb
+++ app/channels/chatrooms_channel.rb
@@ -1,6 +1,10 @@
 class ChatroomsChannel < ApplicationCable::Channel
     def subscribed
+      # generic channel for public chatrooms
       stream_from "chatrooms_channel"
+
+      # private channel for each user
+      stream_from "current_user_#{current_user.id}"
     end
   
     def unsubscribed

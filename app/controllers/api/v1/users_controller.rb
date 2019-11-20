137be9ddf901d1bba02f041ad90d7dc776a96480
oliburt <oliburt@gmail.com>
2019-11-20 17:47:20 +0000

    set token in cookie on create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-            render json: { token: issue_token({user_id: user.id}), user: UserSerializer.new(user) }
+            jwt_token = issue_token(user_id: user.id)
+            cookies.signed[:jwt] = {value: jwt_token, httponly: true, expires: 1.hour.from_now}
+
+            render json: { user: UserSerializer.new(user) }
         else
             render json: { errors: user.errors.full_messages }, status: :not_acceptable
         end

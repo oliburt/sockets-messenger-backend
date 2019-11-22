c28fb7dc9949b56494375294b2a73261245e671c
oliburt <oliburt@gmail.com>
2019-11-22 10:45:41 +0000

    validates unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password 
+    has_secure_password
+    validates :username, uniqueness: true
     has_many :messages 
     has_many :user_chatrooms
     has_many :chatrooms, through: :user_chatrooms

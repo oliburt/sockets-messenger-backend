2a270e3a0970aa980737505cd5425f829bd6a42f
oliburt <oliburt@gmail.com>
2019-11-22 09:24:53 +0000

    add redis and heroku urls for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.web_socket_server_url = "wss://chat-sockets-application.herokuapp.com/"
+  config.action_cable.allowed_request_origins = ['*', '*']
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 

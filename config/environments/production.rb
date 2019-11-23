f6c288e64d2e5188f866508e949cf82aa440e0a6
oliburt <oliburt@gmail.com>
2019-11-23 13:00:37 +0000

    removed local machin from cors and allowed action cable origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.web_socket_server_url = "wss://chat-sockets-application.herokuapp.com/"
-  config.action_cable.allowed_request_origins = [/https:\/\/s-chat-app-frontend.herokuapp.com\/*/, /http:\/\/s-chat-app-frontend.herokuapp.com\/*/, /http:\/\/192.168.1.107:3000\/*/]
+  config.action_cable.allowed_request_origins = [/https:\/\/s-chat-app-frontend.herokuapp.com\/*/, /http:\/\/s-chat-app-frontend.herokuapp.com\/*/]
 
 
   # Code is not reloaded between requests.

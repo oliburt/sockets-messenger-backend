8ca57883abf240cbfdca8b6384d778001f193ff2
oliburt <oliburt@gmail.com>
2019-11-24 19:46:13 +0000

    reject if no token and fixed exception bug

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,12 +9,16 @@ module ApplicationCable
     private
 
     def find_verified_user
-      
-      verified_user = User.find(JWT.decode(cookies.signed[:jwt], ENV['RAILS_SECRET'])[0]["user_id"])
-      if verified_user
-        verified_user
+      token = cookies.signed[:jwt]
+      if token
+        verified_user = User.find_by_id(JWT.decode(token, ENV['RAILS_SECRET'])[0]["user_id"])
+        if verified_user
+          verified_user
+        else
+          cookies.delete(:jwt)
+          reject_unauthorized_connection
+        end
       else
-        cookies.delete(:jwt)
         reject_unauthorized_connection
       end
     end

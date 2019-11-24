9d8aa567265ce147e9dad40ff42a68159a9bba63
oliburt <oliburt@gmail.com>
2019-11-24 16:49:11 +0000

    added presence true validation on username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: true
+    validates :username, presence: true
+
+
     has_many :messages 
     has_many :user_chatrooms
     has_many :chatrooms, through: :user_chatrooms

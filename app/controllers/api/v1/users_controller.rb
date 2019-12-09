25224511c27d7888a5d344fc51c7c74472a17617
oliburt <oliburt@gmail.com>
2019-12-09 12:25:50 +0000

    added accepted status

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,8 +8,7 @@ class Api::V1::UsersController < ApplicationController
         if user.valid?
             jwt_token = issue_token(user_id: user.id)
             cookies.signed[:jwt] = {value: jwt_token, httponly: true, expires: 1.hour.from_now}
-
-            render json: { user: UserSerializer.new(user) }
+            render json: { user: UserSerializer.new(user) }, status: :accepted
         else
             render json: { errors: user.errors.full_messages }, status: :not_acceptable
         end
